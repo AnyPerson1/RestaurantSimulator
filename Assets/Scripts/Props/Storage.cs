79ad6fb3dfe503e2728a9471d9a563baf07e085e
AnyPerson1 <arda45simsek@gmail.com>
2025-02-17 14:16:14 +0300

    add and remove storage

--- Assets/Scripts/Props/Storage.cs
+++ Assets/Scripts/Props/Storage.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
@@ -9,10 +10,13 @@ public class Storage : MonoBehaviour
     [SerializeField] public int currentStorage = 0;
     [SerializeField] public Dictionary<Ingredient.IngredientType,int> storage = new Dictionary<Ingredient.IngredientType, int>();
 
+
     // returns if there is a leftover from fill
     public int AddStorage(Ingredient.IngredientType ingredientType, int amount)
     {
         int leftover = 0;
+        if (currentStorage == maxStorage)
+            return amount;
         if (storage.Keys.Contains(ingredientType))
         {
             if (currentStorage+amount <= maxStorage)
@@ -40,5 +44,28 @@ public class Storage : MonoBehaviour
             }
         }
         return leftover;
+    }
+
+    public int RemoveStorage(Ingredient.IngredientType type, int amount)
+    {
+        int exceed = 0;
+        if (storage.Keys.Contains(type))
+        {
+            if (amount >= storage[type])
+            {
+                exceed = storage[type] - amount;
+                storage.Remove(type);
+            }
+            else
+            {
+                storage[type] -= amount;
+            }
+        }
+        else
+        {
+            Debug.LogError("Removing storage from a storage is completed with an error: There is no any storage with "+type+" key.");
+            return amount;
+        }
+        return exceed;
     }
 }
